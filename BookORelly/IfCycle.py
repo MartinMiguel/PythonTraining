3d1af2d362557513986b37582e9d8f0191b16007
MartinMiguel <xjpsuen@gmail.com>
2018-06-04 23:33:32 -0500

    Last updates jun 04

--- BookORelly/IfCycle.py
+++ BookORelly/IfCycle.py
@@ -6,3 +6,13 @@ else:
     print("False: ",*a)
 a = str(a)[1:-1]
 print("String conversion: ", a)
+print("\n")
+
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
