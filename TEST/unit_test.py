e839d7f5b56c53c02dfffd64ca47d3c50966a214
MartinMiguel <xjpsuen@gmail.com>
2018-06-07 23:40:39 -0500

    07/06/18 Night

--- TEST/unit_test.py
+++ TEST/unit_test.py
@@ -1,13 +1,21 @@
 import unittest
 
 def fun(x):
+    Y=8
+    print("Y output:",Y)
     return x + 1
+
+def capital_case(x):
+    Z=8
+    print("Z output:", Z)
+    return x.capitalize()
+
+
 
 class MyTest(unittest.TestCase):
     def test1(self):
         self.assertEqual(fun(3), 4)
     def test2(self):
         self.assertEqual(capital_case('semaphore'),'semaphore')
-
-def capital_case(x):
-    return x.capitalize()
+    def test3(self):
+        self.assertEqual(fun(1), 2)
