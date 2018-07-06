e781536da05f6c1f63068dd41548b764cdc62554
MartinMiguel <xjpsuen@gmail.com>
2018-07-06 18:46:29 -0500

    Ship

--- CODILITY/example3.py
+++ CODILITY/example3.py
@@ -1,70 +1,65 @@
 def solution(N, S, T):
-    # write your code in Python 3.6
-    print(N)
-    print(S)
-    print(T)
+    #write your code in Python 3.6
+    #Print Received N,S,T
+    print("Table:",N)
+    print("Corner ship points:",S)
+    print("Hits:",T)
 
-    A = [0] * N
-    for i in range(N):
-        A[i] = [0] * N
-    print(A)
-    SN=S[::3]
-    SL=S[1::3]
-    print("Slicing: S", S[::3])
-    print("Slicing: S", SL)
-    TN = T[::3]
-    TL = T[1::3]
-    print("Slicing: T", T[::3])
-    print("Slicing: T", TL)
+    # creating table
+    Table = [[0 for x in range(N)] for x in range(N)]
+    print("Initializing Matrix Table:",Table)
 
-    dict = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8,
+    # slicing to separate strings from numbers
+    # Change from string to int
+    SRows=S[::3]
+    #SRows = int(''.join(str(e) for e in S[::3]))
+    #SRows = int(''.join(str(e) for e in S[::3]))
+    #SRows = list(map(int, S[::3]))
+    SCols=S[1::3]
+    #SCols = int(''.join(str(e) for e in S[1::3]))
+    print("S Rows:", SRows)
+    print("S Cols:", SCols)
+    TRows = T[::3]
+    #TRows = int(''.join(str(e) for e in T[::3]))
+    TCols = T[1::3]
+    print("T Rows", TRows)
+    print("T Cols", TCols)
+
+    #Map Columns to Numbers
+    ColsMap = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8,
              'I': 9, 'J': 10, 'K': 11, 'L': 12, 'M': 13, 'N': 14, 'O': 15, 'P': 16, 'Q': 17,
              'R': 18, 'S': 19, 'T': 20, 'U': 21, 'V': 22, 'W': 23, 'X': 24, 'Y': 25, 'Z': 26
              }
-    NewSL=[]
-    for i in SL:
-       # print(i)
-        NewSL.append(dict[i])
-    print(NewSL)
-    NewTL=[]
-    for i in TL:
-       # print(i)
-        NewTL.append(dict[i])
-    print(NewTL)
+    SColsNum=[]
+    for i in SCols:
+        SColsNum.append(ColsMap[i])
+    #Changing from list to string to integer
+    SColsNum = ''.join(str(e) for e in SColsNum)
+    print("Mapped S Colums Letters to Numbers:", SColsNum)
+    TColsNum=[]
+    for i in TCols:
+        TColsNum.append(ColsMap[i])
+    # Changing from list to string to integer
+    TColsNum = ''.join(str(e) for e in TColsNum)
+    print("Mapped T Colums Letters to Numbers:",TColsNum)
 
-    #Fill ships
-    for i in SN[::2]:
+
+    #Ships filled
+    x=0
+    for k in range(0,int(len(SRows)/2)):
+        for i in range(int(SRows[x]),int(SRows[x+1])+1):
+             for j in range(int(SColsNum[x]),int(SColsNum[x+1])+1):
+                 Table[i-1][j-1]=1
+        x=x+2
+    print("Ships filled:",Table)
+
+    # Hits filled
+    for i in range(int(len(TRows))):
+        Table[int(TRows[i])-1][int(TColsNum[i])-1] = 2
         #print(i)
-        #for i in range(A):
-           #for j in range(A):
-               #print(i,j)
-
-        for j in NewSL[::2]:
-            print(i,j)
-        #for i in range(A):
-           #for j in range(A):
-               #print(i,j)
-
-
-
-            #Y=list(dict.values())
-    #if 1 in Y:
-        #print("Yes")
-        # print("Yes")
-    #print(Y)
-    #a = []
-    #for i in S:
-        #print("Hey:",i)
-        #print(S)
-        #print(dict.values())
-        #if i in Y:
-            #a.append(i)
-            #print("Yes",i)
-            #break
-    #print(a)
-        #a.append(i)
-   # a  # the list with the new items.
-
+        #print("Hits filled:", Table)
+    #Table[1][2] = 2
+    print("Hits filled:", Table)
     pass
 
 N = 4
--- CODILITY/example4.py
+++ CODILITY/example4.py
@@ -1,6 +1,11 @@
-import numpy as np
+def identity(n):
+    m=[[0 for x in range(n)] for y in range(n)]
+    for i in range(0,2):
+        print("i,n",i,n)
+        m[i][i] = 1
+    print(m)
 
-A = np.array([1, 2, 3])
+identity(3)
 
-N = A.size
-B = np.pad(np.diag(A), ((0, N), (0, 0)), mode='constant')
+for x in range(2, 6):
+  print(x)
